e88936f79c8087983dac6b1a762656c852db0f01
JoshGutman <jng86@nau.edu>
2016-12-31 16:41:12 -0800

    Added division

--- GF.h
+++ GF.h
@@ -12,14 +12,15 @@ public:
 	GF(string);
 	unsigned int multiply(unsigned int, unsigned int);
 	unsigned int mod(unsigned int, unsigned int);
+	unsigned int divide(unsigned int, unsigned int);
 
 private:
 	unsigned int mostSignificantBit(unsigned int);
 	string removeAll(char, string);
+	int eea(int, int);
+	int* ea(int, int);
+	int ea_recurse(int, int, int, int, int);
+
 	unsigned int px;
 
 };
-
-
-
-#endif
